1036e4bc3624d919cc0b04b52b5de2994e2671ae
Rodrigo82Araya <araya.r82@gmail.com>
2022-08-06 18:43:29 -0400

    listado stardar

--- app_orden/views.py
+++ app_orden/views.py
@@ -7,20 +7,29 @@ from .models import Orden
 #ORDENES====================================================================================
 def orden_listar(request):
     ordenes_all = Orden.objects.all().filter(activo=True)
-    pages = request.GET.get('page',1)
-   
+    filas = 4
+    pages = request.GET.get('page',1)    
         
     try:
-        paginator = Paginator(ordenes_all, 5)
+        paginator = Paginator(ordenes_all, filas)
         ordenes = paginator.page(pages)
-        
+        filaInicio= (filas * ordenes.number) - (filas - 1)
+
+        filasTotales = (filas * ordenes.number)  
+        if ( filasTotales < ordenes_all.count()): 
+            filaFin=filasTotales
+        else:
+            filaFin=ordenes_all.count()
+
     except:
         raise Http404
 
     data={
         'entity':ordenes,
         'paginator':paginator,
-        'cantidad':ordenes_all.count(),
+        'totalFilas':ordenes_all.count(),
+        'filaInicio': filaInicio,
+        'filaFin':filaFin,
     }
     return render(request, 'orden/orden_listar.html',data)
 
