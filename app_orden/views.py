fc8f1376c336d3b91993fc23fecb9958451559a0
Rodrigo82Araya <araya.r82@gmail.com>
2022-08-08 01:38:29 -0400

    unificacion de ventanas

--- app_orden/views.py
+++ app_orden/views.py
@@ -5,6 +5,9 @@ from app_orden.forms import OrdenesForm
 from .models import Orden
 
 #ORDENES====================================================================================
+titulo="Orden"
+tituloPlu="Ordenes"
+
 def orden_listar(request):
     ordenes_all = Orden.objects.all().filter(activo=True).order_by('id').reverse()
     filas = 7
@@ -30,12 +33,18 @@ def orden_listar(request):
         'totalFilas':ordenes_all.count(),
         'filaInicio': filaInicio,
         'filaFin':filaFin,
+        'pageTitulo': titulo,
+        'pageTituloPlu': tituloPlu,
+        'pageAccion': 'Listado',
     }
     return render(request, 'orden/orden_listar.html',data)
 
 def orden_agregar(request):
     data = {
-        'form': OrdenesForm()
+        'form': OrdenesForm(),
+        'pageTitulo': titulo,
+        'pageTituloPlu': tituloPlu,
+        'pageAccion': 'Agregar',
     }
 
     if request.method == 'POST':
@@ -49,18 +58,22 @@ def orden_agregar(request):
             data["msg_text"] = "Problemas para guardar la orden"
             data["msg_tipo"] = 2
 
-    return render(request, 'orden/orden_agregar.html',data)
+    return render(request, 'orden/orden_agregar_modificar.html',data)
 
 def orden_modificar(request, id):
     ordenes = get_object_or_404(Orden, id=id)
     data = {
-        'form':OrdenesForm(instance=ordenes)
+        'form':OrdenesForm(instance=ordenes),
+        'pageTitulo': titulo,
+        'pageTituloPlu': tituloPlu,
+        'pageAccion': 'Modificar',
     }
 
     if request.method == 'POST':
         formulario = OrdenesForm(data=request.POST, instance=ordenes, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
+            return redirect(to="orden_listar")
             data["msg_text"] = "Orden modificada correctamente"
             data["msg_tipo"] = 1
         else:
@@ -68,7 +81,7 @@ def orden_modificar(request, id):
             data["msg_text"] = "Problemas para guardar la orden"
             data["msg_tipo"] = 2
 
-    return render(request, 'orden/orden_modificar.html', data)
+    return render(request, 'orden/orden_agregar_modificar.html', data)
 
 def orden_eliminar(request, id):
     ordenes = get_object_or_404(Orden, id=id)
---
+++ proj_control/static/js/script.js
@@ -0,0 +1,30 @@
+function alertConfirmacion(e) {
+    e.preventDefault();
+    
+    var url = e.currentTarget.getAttribute('href');
+    Swal.fire({
+        title: 'Realmente deseas eliminar esta orden?',
+        text: "Esto no se podra revertir!",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        
+        if (result.value) {
+            window.location.href=url;
+        }else {
+            return false;
+        }
+    })
+}
+
+function alertExito(e){
+    Swal.fire({
+        title: 'Realmente deseas eliminar esta orden?',
+        text: "Esto no se podra revertir!",
+        icon: 'success',
+    })  
+}
