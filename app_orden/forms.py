d98ee02d2e98ebd7c7604bd1f30a0c8fb5c3db2e
Rodrigo82Araya <araya.r82@gmail.com>
2022-08-08 19:33:05 -0400

    orden formulario modificar

--- app_orden/forms.py
+++ app_orden/forms.py
@@ -12,16 +12,20 @@ class OrdenesForm(forms.ModelForm):
         model = Orden
         fields = ['nombre','descripcion','prioridad']
         widgets = {
-            'nombre': TextInput(attrs={
-                'class': "form-control",
-                'placeholder': 'Ingrese el nombre de la orden'
-            }),
             'descripcion': Textarea(attrs={
                 'class': "form-control", 
                 'placeholder': 'Ingrese el detalle de la orden'
-            }),
-            'prioridad':forms.widgets.CheckboxInput(attrs={
-                'class': "form-check-input"
-            })
+            }),            
         }
+
+    def __init__(self, *args, **kwargs):
+        super(OrdenesForm, self).__init__(*args,**kwargs)
+        self.fields['nombre'].widget.attrs['placeholder'] = 'Ingrese el nombre de la orden'
+        self.fields['prioridad'].widget.attrs['class'] = 'form-check-input'        
+
+        for field in self.fields:
+            if field != 'prioridad':
+                self.fields[field].widget.attrs['class']='form-control'       
+        
+        
         
--- app_orden/views.py
+++ app_orden/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 from app_orden.forms import OrdenesForm
 from .models import Orden
+from django.contrib import messages
 
 #ORDENES====================================================================================
 titulo="Orden"
@@ -51,12 +52,12 @@ def orden_agregar(request):
         formulario = OrdenesForm(data=request.POST, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
-            data["msg_text"] = "Orden guardada correctamente"
-            data["msg_tipo"] = 1
+            messages.success(request,"Orden ingresada correctamente")
+
+            redirect("orden_agregar")
         else:
             data["form"] = formulario
-            data["msg_text"] = "Problemas para guardar la orden"
-            data["msg_tipo"] = 2
+            messages.error(request, 'Problemas para ingresar la orden')
 
     return render(request, 'orden/orden_agregar_modificar.html',data)
 
@@ -73,13 +74,12 @@ def orden_modificar(request, id):
         formulario = OrdenesForm(data=request.POST, instance=ordenes, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
-            return redirect(to="orden_listar")
-            data["msg_text"] = "Orden modificada correctamente"
-            data["msg_tipo"] = 1
+            messages.success(request,"Orden modificada correctamente")
+
+            return redirect("orden_modificar",id)
         else:
+            messages.error(request, 'Problemas para modificar la orden')
             data["form"] = formulario
-            data["msg_text"] = "Problemas para guardar la orden"
-            data["msg_tipo"] = 2
 
     return render(request, 'orden/orden_agregar_modificar.html', data)
 
@@ -87,4 +87,3 @@ def orden_eliminar(request, id):
     ordenes = get_object_or_404(Orden, id=id)
     ordenes.delete()
     return redirect(to='orden_listar')
-    #return render(request, 'orden/orden_listar.html')
