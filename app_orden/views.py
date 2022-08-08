3b313b825f0da2a7f047e1e4942fc5132ea0a6d2
Rodrigo82Araya <araya.r82@gmail.com>
2022-08-07 22:38:10 -0400

    ordenes formulario agregar

--- app_orden/views.py
+++ app_orden/views.py
@@ -42,9 +42,12 @@ def orden_agregar(request):
         formulario = OrdenesForm(data=request.POST, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
-            data["mensaje"] = "Guardado correctamente"
+            data["msg_text"] = "Orden guardada correctamente"
+            data["msg_tipo"] = 1
         else:
             data["form"] = formulario
+            data["msg_text"] = "Problemas para guardar la orden"
+            data["msg_tipo"] = 2
 
     return render(request, 'orden/orden_agregar.html',data)
 
@@ -58,10 +61,12 @@ def orden_modificar(request, id):
         formulario = OrdenesForm(data=request.POST, instance=ordenes, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
-            #data["mensaje"] = "Modificado correctamente"
-            return redirect(to="orden_listar")
+            data["msg_text"] = "Orden modificada correctamente"
+            data["msg_tipo"] = 1
         else:
-            data["form"] = formulario          
+            data["form"] = formulario
+            data["msg_text"] = "Problemas para guardar la orden"
+            data["msg_tipo"] = 2
 
     return render(request, 'orden/orden_modificar.html', data)
 
